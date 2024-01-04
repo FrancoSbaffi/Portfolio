efdc0ab663b96e9425dd0210ace4bd6dbdccdb62
Franco Sbaffi <francosbaffidev@gmail.com>
2024-01-04 19:15:58 -0300

    background finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 
-  //  function bgAnimationItems(){
-  //     const rows = 7, cols = 10;
-  //     for(let i = 0; i < rows; i++){
-  //         for(let j = 0; j < cols; j++){
-  //              const div = document.createElement("div");
-  //              div.className = `col-${j+1}`; 
-  //              document.querySelector(".bg-animation-effect").appendChild(div); 
-  //          }
-  //    }
-  //  }
-  // bgAnimationItems();
+ function bgAnimationItems(){
+    const rows = 7, cols = 10;
+    for(let i = 0; i < rows; i++){
+        for(let j = 0; j < cols; j++){
+             const div = document.createElement("div");
+             div.className = `col-${j+1}`; 
+             document.querySelector(".bg-animation-effect").appendChild(div); 
+         }
+   }
+ }
+bgAnimationItems();
 
 // DOCK
 
